8a6cbb4fb06e0410c9d7f7ecf227b4ffeb0b6fc2
youssef <https://github.com/youssefna134@gmail.com-collab/FinalProject.git>
2025-12-14 16:54:26 -0500

    department class finished with proper documentation

--- src/main/java/org/youssefn/Department.java
+++ src/main/java/org/youssefn/Department.java
@@ -1,10 +1,51 @@
 package org.youssefn;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@EqualsAndHashCode
+@Getter
 public class Department {
     private String departmentId;
-    private String departmentName;
+    @Setter private String departmentName;
     private static int nextId = 1;
 
+    /**
+     * checks if department name is valid:
+     * it should only contain letters or spaces
+     * @param departmentName the department name input
+     * @return true if department name is valid and false if it is not
+     */
+    public static boolean isDepartmentNameValid(String departmentName) {
+        if (departmentName == null) {
+            return false;
+        }
+        for (int i = 0; i < departmentName.length(); i++) {
+            char currentChar = departmentName.charAt(i);
+            if (!Character.isLetter(currentChar)) {
+                if (currentChar != ' ') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private static String generateNewId() {
+        return String.format("D%02d", nextId++);
+    }
+
+    public Department(String departmentName) {
+        if (!isDepartmentNameValid(departmentName)) {
+            this.departmentName = null;
+            this.departmentId = null;
+        }
+        else {
+            this.departmentName = departmentName;
+            this.departmentId = generateNewId();
+        }
+    }
 }
-
-
