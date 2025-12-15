b1fba242bbccf276a103600001ea1d452cbcca43
youssef <https://github.com/youssefna134@gmail.com-collab/FinalProject.git>
2025-12-14 21:50:45 -0500

    Finished util class method + proper documentation

--- src/main/java/Util/Util.java
+++ src/main/java/Util/Util.java
@@ -1,5 +1,31 @@
 package Util;
 
 public class Util {
+    /**
+     * Converts each word of a string into title case.
+     * @param str the string input
+     * @return every word of the string in title case
+     */
+    public static String toTitleCase(String str) {
+        if (str == null || str.isEmpty()) {
+            return str;
+        }
 
+        String[] words = str.split(" ");
+        String titleCaseString = "";
+
+        for (String word : words) {
+            String titleCaseWord = word.substring(0, 1).toUpperCase()
+                        + word.substring(1).toLowerCase();
+
+            if (!titleCaseString.isEmpty()) {
+                titleCaseString += " ";
+            }
+
+            titleCaseString += titleCaseWord;
+        }
+
+        return titleCaseString;
+    }
 }
+
